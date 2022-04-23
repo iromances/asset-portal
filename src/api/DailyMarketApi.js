f990162c489c83c84738443bed8c61dfefe86045
shihao <ymcdhl@163.com>
2022-04-23 20:19:51 +0800

    123456

--- src/api/DailyMarketApi.js
+++ src/api/DailyMarketApi.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function fetchList(query) {
   return request({
-    url: 'http://localhost:8888/asset/trade/record/index',
+    url: 'http://localhost:8888/asset/trade/daily/market/index',
     method: 'get',
     params: query
   })
--- src/views/trade/market/index.vue
+++ src/views/trade/market/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.serialNo" placeholder="流水号" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-input v-model="listQuery.stockCode" placeholder="代码" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-input v-model="listQuery.stockName" placeholder="名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.status" placeholder="交易状态" clearable style="width: 120px" class="filter-item">
-        <el-option v-for="item in statusArray" :key="item.key" :label="item.label" :value="item.value" />
-      </el-select>
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         查询
       </el-button>
@@ -22,38 +18,50 @@
       style="width: 100%;"
       @sort-change="sortChange"
     >
-      <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80" :class-name="getSortClass('id')" />
-      <el-table-column label="流水" prop="serialNo" sortable="custom" align="center" width="300" :class-name="getSortClass('id')" />
-      <el-table-column label="资产类型" prop="stockType" align="center" />
+      <el-table-column label="ID" prop="id" align="center" />
+      <el-table-column label="排名" prop="seq" align="center" />
+      <el-table-column label="交易日" prop="tradeDate" width="100" align="center" />
       <el-table-column label="代码" prop="stockCode" align="center" />
       <el-table-column label="名称" prop="stockName" width="250" align="center" />
-      <el-table-column label="交易方向" prop="direction" align="center" />
-      <el-table-column label="下单价格" prop="orderPrice" align="center" />
-      <el-table-column label="下单数量" prop="orderNum" align="center" />
-      <el-table-column label="交易状态" prop="status" align="center" />
-      <el-table-column label="已成交" prop="avgPrice" align="center" />
-      <el-table-column label="下单时间" prop="orderTime" width="170" align="center" />
-      <el-table-column label="订单类型" width="120px" prop="orderType" align="center" />
-      <el-table-column label="期限" prop="periodValidity" align="center" />
-      <el-table-column label="盘前盘后" prop="preOrAfterMarket" align="center" />
-      <el-table-column label="触发价" prop="triggerPrice" align="center" />
-      <el-table-column label="卖空" prop="sellingShort" align="center" />
-      <el-table-column label="订单来源" prop="orderSource" align="center" />
-      <el-table-column label="成交数量" prop="dealNum" align="center" />
-      <el-table-column label="成交价格" prop="dealPrice" align="center" />
-      <el-table-column label="成交金额" prop="dealAmount" align="center" />
-      <el-table-column label="成交时间" prop="dealTime" width="170" align="center" />
+      <el-table-column label="所属行业" prop="industry" width="100" align="center" />
 
-      <el-table-column label="佣金" prop="brokerage" align="center" />
-      <el-table-column label="平台使用费" width="90" prop="platformFee" align="center" />
-      <el-table-column label="交收费" prop="stockSettlementFee" align="center" />
-      <el-table-column label="证监会规费" width="90" prop="sfcFees" align="center" />
-      <el-table-column label="交易活动费" width="90" prop="transActivityFee" align="center" />
-      <el-table-column label="期权清算费" width="90" prop="optionClearingFee" align="center" />
-      <el-table-column label="交易所费用" width="90" prop="exchangeCost" align="center" />
-      <el-table-column label="合计费用" prop="totalAmount" align="center" />
+      <el-table-column label="最新价" prop="latestPrice" align="center" />
+      <el-table-column label="涨跌额" prop="riseFallAmount" align="center" />
+      <el-table-column label="涨跌幅" prop="riseFallPercent" align="center" />
 
-      <el-table-column label="订单匹配标识" width="120" prop="matchFlag" align="center" />
+      <el-table-column label="成交量" prop="volume" align="center" />
+      <el-table-column label="成交额" prop="turnover" width="170" align="center" />
+      <el-table-column label="总市值" width="120px" prop="marketValue" align="center" />
+      <el-table-column label="离52周高点百分比" prop="highPointWeek52" align="center" />
+      <el-table-column label="离52周低点百分比" prop="lowPointWeek52" align="center" />
+      <el-table-column label="MA50" prop="day50ma" align="center" />
+      <el-table-column label="MA150" prop="day150ma" align="center" />
+      <el-table-column label="MA200" prop="day200ma" align="center" />
+      <el-table-column label="买入价" prop="buyPrice" align="center" />
+      <el-table-column label="卖出价" prop="sellPrice" align="center" />
+
+      <el-table-column label="买量" prop="buyVolume" align="center" />
+      <el-table-column label="卖量" prop="sellVolume" align="center" />
+
+      <el-table-column label="今开" prop="todayOpeningPrice" align="center" />
+      <el-table-column label="昨收" prop="lastDayClosingPrice" align="center" />
+      <el-table-column label="最高" prop="highestPrice" align="center" />
+      <el-table-column label="最低" prop="lowestPrice" align="center" />
+      <el-table-column label="量比" prop="liangbi" align="center" />
+      <el-table-column label="委比" prop="weibi" align="center" />
+      <el-table-column label="振幅" prop="zhenfu" align="center" />
+      <el-table-column label="市盈率" prop="pe" align="center" />
+
+      <el-table-column label="换手率" prop="turnoverRate" align="center" />
+      <el-table-column label="涨跌速率" prop="riseFallSpeedPercent" align="center" />
+      <el-table-column label="5分钟涨跌幅" prop="min5RiseFallPercent" align="center" />
+      <el-table-column label="5日涨跌幅" prop="day5RiseFallPercent" align="center" />
+      <el-table-column label="10日涨跌幅" prop="day10RiseFallPercent" align="center" />
+      <el-table-column label="20日涨跌幅" prop="day20RiseFallPercent" align="center" />
+      <el-table-column label="60日涨跌幅" prop="day60RiseFallPercent" align="center" />
+      <el-table-column label="120日涨跌幅" prop="day120RiseFallPercent" align="center" />
+      <el-table-column label="250日涨跌幅" prop="day250RiseFallPercent" align="center" />
+      <el-table-column label="年初至今涨跌幅" prop="thisYearRiseFallPercent" align="center" />
     </el-table>
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
@@ -61,40 +69,12 @@
 </template>
 
 <script>
-import { fetchList } from '@/api/tradeHistoryDetailApi'
-import waves from '@/directive/waves' // waves directive
+import { fetchList } from '@/api/DailyMarketApi'
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
-
-const calendarTypeOptions = [
-  { key: 'CN', display_name: 'China' },
-  { key: 'US', display_name: 'USA' },
-  { key: 'JP', display_name: 'Japan' },
-  { key: 'EU', display_name: 'Eurozone' }
-]
-
-// arr to obj, such as { CN : "China", US : "USA" }
-const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
-  acc[cur.key] = cur.display_name
-  return acc
-}, {})
 
 export default {
   name: 'DayilyMarket',
   components: { Pagination },
-  directives: { waves },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'info',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    },
-    typeFilter(type) {
-      return calendarTypeKeyValue[type]
-    }
-  },
   data() {
     return {
       tableHeight: document.documentElement.clientHeight - 309 + 'px',
@@ -104,23 +84,8 @@ export default {
       listLoading: true,
       listQuery: {
         page: 1,
-        limit: 20,
-        importance: undefined,
-        title: undefined,
-        type: undefined,
-        sort: '+id'
+        limit: 20
       },
-      importanceOptions: [1, 2, 3],
-      calendarTypeOptions,
-      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
-      statusOptions: ['published', 'draft', 'deleted'],
-      showReviewer: false,
-      pvData: [],
-      statusArray: [
-        { key: '1', label: '全部成交', value: '全部成交' },
-        { key: '2', label: '已撤单', value: '已撤单' },
-        { key: '3', label: '部成已撤', value: '部成已撤' }
-      ],
       rules: {
         type: [{ required: true, message: 'type is required', trigger: 'change' }],
         timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
@@ -162,10 +127,6 @@ export default {
         this.listQuery.sort = '-id'
       }
       this.handleFilter()
-    },
-    getSortClass: function(key) {
-      const sort = this.listQuery.sort
-      return sort === `+${key}` ? 'ascending' : 'descending'
     }
   }
 }
