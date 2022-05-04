df79ba18e56504ae6813af46f2542ac3a535abdb
shihao <ymcdhl@163.com>
2022-05-04 22:50:15 +0800

    投资数据面板开发

--- src/views/trade/dashboard/admin/components/TransactionTable.vue
+++ src/views/trade/dashboard/admin/components/TransactionTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="list" stripe border show-summary	sum-text="合计" style="width: 100%;padding-top: 15px;">
+  <el-table :data="list" stripe border show-summary :summary-method="getSummaries" style="width: 100%;padding-top: 15px;">
     <el-table-column label="日期" prop="tradeMonth" width="120" align="center" />
     <el-table-column label="平均收益" prop="avgYield" align="center">
       <template slot-scope="scope">
@@ -50,7 +50,8 @@ export default {
   },
   data() {
     return {
-      list: null
+      list: null,
+      summaryData: {}
     }
   },
   created() {
@@ -66,6 +67,52 @@ export default {
       }).then(response => {
         this.list = response.data.records
       })
+
+      request({
+        url: 'http://localhost:8888/asset/monthly/trade/summary/report',
+        method: 'get',
+        param: data
+      }).then(response => {
+        this.summaryData = response.data
+      })
+    },
+    // 金额合计
+    getSummaries(param) {
+      var _this = this
+      const { columns } = param
+      const sums = []
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '合计'
+          return
+        }
+        switch (column.property) {
+          case 'avgYield':
+          case 'avgLossRate':
+          case 'avgAccuracy':
+            sums[index] = '-'
+            break
+          case 'tradeNum':
+            sums[index] = _this.summaryData.tradeNum ? _this.summaryData.tradeNum + '%' : ''
+            break
+          case 'maxYield':
+            sums[index] = _this.summaryData.maxYield ? _this.summaryData.maxYield + '%' : ''
+            break
+          case 'maxLoss':
+            sums[index] = _this.summaryData.maxLoss ? _this.summaryData.maxLoss + '%' : ''
+            break
+          case 'yieldDay':
+            sums[index] = _this.summaryData.yieldDay ? _this.summaryData.yieldDay : ''
+            break
+          case 'lossDay':
+            sums[index] = _this.summaryData.lossDay ? _this.summaryData.lossDay : ''
+            break
+          default:
+            sums[index] = ''
+            break
+        }
+      })
+      return sums
     }
   }
 }
